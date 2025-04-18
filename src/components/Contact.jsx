4deb4ebbc1f5b88954d9af24fcf1b5f682541013
dBillionaireDev <nezerekunke@gmail.com>
2025-04-18 06:06:41 +0100

    Landing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div className='w-[94%] lg:h-[20vh] bg-green-600 m-[auto] lg:px-15 px-5 py-5'>
+        <div className='w-[94%] lg:h-[20vh]  m-[auto] lg:px-15 px-5 pt-5 pb-3 flex lg:flex-row flex-col lg:gap-x-90 lg:mb-3'>
             <div>
                 <h2 className="lg:text-7xl text-5xl font-normal tracking-wide mb-2">Get In Touch</h2>
-                <p>Feel fee to contact me for any project or collaboration</p>
+                <p className='lg:text-3xl font-light lg:mb-0 mb-5'>Feel fee to contact me for any project or collaboration</p>
             </div>
             <div>
-                <button>Contact Me</button>
+                <button className="bg-black hover:bg-gray-400 text-white hover:text-black py-4 px-8 lg:pt-4 lg:pb-6 mb-5 rounded-2xl lg:text-3xl text-2xl font-normal self-center ">Contact Me</button>
             </div>
         </div>
     )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className='flex justify-center m-[auto] w-[100%]'>
             <small>
-                © Eliezer Ekunke {new Date().getFullYear()} by <a href="https://github.com/dBillionaireDev" target="_blank">Nexy</a>
+                © {new Date().getFullYear()} Eliezer Ekunke by <span className='text-blue-300'><a href="https://github.com/dBillionaireDev" target="_blank">Nexy</a></span>
             </small>
         </footer>
 
