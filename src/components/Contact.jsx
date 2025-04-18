2994f0226eff6dee9ed377c681dd2947d8058b39
dBillionaireDev <nezerekunke@gmail.com>
2025-04-18 05:48:32 +0100

    moving

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <>
+        <div className='w-[94%] lg:h-[20vh] bg-green-600 m-[auto] lg:px-15 px-5 py-5'>
             <div>
-                <h2>Get In Touch</h2>
+                <h2 className="lg:text-7xl text-5xl font-normal tracking-wide mb-2">Get In Touch</h2>
                 <p>Feel fee to contact me for any project or collaboration</p>
             </div>
             <div>
                 <button>Contact Me</button>
             </div>
-        </>
+        </div>
     )
 }
 
