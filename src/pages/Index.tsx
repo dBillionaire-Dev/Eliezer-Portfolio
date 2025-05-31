75f9520e7eeb812ade2cb0c56a3f5610dc3271b9
Nex.Dev <nezerekunke@gmail.com>
2025-05-31 06:22:05 +0100

    100

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,9 @@
+import Home from 'pages/Home.tsx';
+
 const Index = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="text-center">
-      </div>
+    <div>
+      <Home />
     </div>
   );
 };
