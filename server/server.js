734d3a6c72f06f934e92c3e6cd49260cc8e5643b
dBillionaire-Dev <nezerekunke@gmail.com>
2025-11-09 23:50:19 +0100

    Fix CORS issue

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({ 
-  origin: 'http://localhost:8080', 
+  origin: 'https://eliezer-portfolio.vercel.app',
   credentials: true 
 })
 ); 
